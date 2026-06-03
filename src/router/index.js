a2187952bc9da7947c76ab03b0339c1baf8b0c8c
qingxin.jiang <qingxin2016@gmail.com>
2026-06-03 21:19:10 +0800

    chore: update router for vite env

--- src/router/index.js
+++ src/router/index.js
@@ -10,23 +10,17 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
   },
   {
     path: '/editor',
     name: 'editor',
-    // route level code-splitting
-    // this generates a separate chunk (editor.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "editor" */ '../views/EditorView.vue')
+    component: () => import('../views/EditorView.vue')
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
